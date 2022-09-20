bd92f36d265a824e1c6c5bf0ef4a7b46b49d34fb
Kurtiadam <adamkurti97@gmail.com>
2022-09-20 11:12:22 +0200

    Added automatic stepping

--- Namco/pacman.py
+++ Namco/pacman.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+from pytimedinput import timedInput
 
 class PacMan:
     def __init__(self, map_size=10, max_time_step=100):
@@ -14,7 +14,7 @@ class PacMan:
     def reset(self):
         pass
 
-    def step(self):
+    def step(self, action):
         done = False
 
         # Increasing the timestep
@@ -29,14 +29,26 @@ class PacMan:
     def render(self):
         pass
 
+    # Automatic stepping if no input (direction) given
+    def auto_step(self, prev_action):
+        a, timedOut = timedInput("Choose your next action:\n")
+        if timedOut:
+            done_ = env.step(action=prev_action)
+        else:
+            done_ = env.step(action=int(a))
+            prev_action = a
+        return prev_action
 
 if __name__ == "__main__":
     # Instantiating the environment
     env = PacMan(map_size=10,
                  max_time_step=100)
-
     done_ = False
+    # First step
+    a = int(input("Choose your first action:\n"))
+    state, reward, done_, info = env.step(action=a)
+    prev_action = a
     while not done_:
-        done_ = env.step()
+        done_ = env.auto_step(prev_action)
 
 
